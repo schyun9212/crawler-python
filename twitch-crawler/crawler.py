f3bcef35ab3ae02f9a2201d52d31cabb1f24ebec
schyun <schyun9212@gmail.com>
2018-11-23 03:16:48 +0900

    implement twitch crawler

--- twitch-crawler/crawler.py
+++ twitch-crawler/crawler.py
@@ -1,50 +1,111 @@
 import requests
+import os
+import json
 
 class Crawler:
-    def __init__(self, client_id, streamers):
-        self.base_url = 'https://api.twitch.tv/helix'
+    def __init__(self, client_id, client_secret):
+        self.base_url = 'https://api.twitch.tv/kraken'
         self.client_id = client_id
-        self.streamers = streamers
+        self.client_secret = client_secret
         self.headers = {
-            'Client-ID' : client_id
+            'Client-ID' : client_id,
+            'Accept' : 'application/vnd.twitchtv.v5+json'
         }
-        
-        self.login = self._login(streamers)
-        self.streamer_ids = {}
-
-    def _login(self, streamers):
-        login = '?'
-        for s in streamers:
-            login += ('login=' + s + '&')
-        return login
-
-    def _streamer_ids(self, response):
-        ids = {}
-        for datum in response.json()['data']:
-            ids[datum['login']] = datum['id']
-        return ids
-
-    def _users(self):
-        res = requests.get(self.base_url + '/users' + self.login, headers = self.headers)
-        self.users = res.json()
-
-        for datum in self.users['data']:
-            self.streamer_ids[datum['login']] = datum['id']
     
-    def _follows(self):
-        for sid in self.streamer_ids.values():
-            res = requests.get(self.base_url + '/users/follows?from_id=' + sid, headers = self.headers)
-            print(res.json())
+    def _oauth(self, scope):
+        res = requests.post('https://id.twitch.tv/oauth2/token?'
+                    + 'client_id=' + self.client_id
+                    + '&client_secret=' + self.client_secret
+                    + '&grant_type=client_credentials'
+                    + '&scope=' + scope)
+        return res.json()['access_token']
 
-    def Run(self):
-        self._users()
-        self._follows()
-        print(self.users)
-        print(self.streamer_ids)
+    def _login(self, names):
+        login = '?login='
+        for name in names:
+            login += (name + ',')
+        return login[:-1]
+    
+    def Users(self, names):
+        query = self._login(names)
+        res = requests.get(self.base_url + '/users' + query, headers = self.headers)
+        return res.json()['users']
+
+    def Channel_by_ID(self, users):
+        channels = {}
+        for user in users:
+            res = requests.get(self.base_url + '/channels/' + user['_id'], headers = self.headers)
+            channel = res.json()
+            channels[user['display_name']] = channel
+        return channels
+
+    def Follows(self, users):
+        follows = {}
+        for user in users:
+            res = requests.get(self.base_url + '/users/' + user['_id'] + '/follows/channels?limit=100', headers = {'Client-ID' : self.client_id })
+            follow =  res.json()
+            follows[user['display_name']] = follow
+        return follows
+
+    def Teams(self, users):
+        teams = {}
+        for user in users:
+            res = requests.get(self.base_url + '/channels/' + user['_id'] + '/teams', headers = self.headers)
+            team = res.json()
+            teams[user['display_name']] = team
+        return teams
+
+    def Subscribers(self, users):
+        auth_headers = {
+            'Client-ID' : client_id,
+            'Accept' : 'application/vnd.twitchtv.v5+json',
+            'Authorization' : 'OAuth ' + self._oauth('channel_subscriptions') 
+        }
+        subscribers = {}
+        for user in users:
+            res = requests.get(self.base_url + '/channels/' + user['_id'] + '/subscriptions', headers = auth_headers)
+            subscriber = res.json()
+            subscribers[user['display_name']] = subscriber
+        return subscribers
+    
+    def Videos(self, users):
+        videos = {}
+        for user in users:
+            res = requests.get(self.base_url + '/channels/' + user['_id'] + '/videos', headers = self.headers)
+            video = res.json()['videos']
+            videos[user['display_name']] = video
+        return videos
+
+    def Stream_by_User(self, users):
+        streams = {}
+        for user in users:
+            res = requests.get(self.base_url + '/streams/' + user['_id'], headers = self.headers)
+            stream = res.json()['videos']
+            streams[user['display_name']] = stream
+        return streams
+    
+    def Top_Game(self):
+        res = requests.get(self.base_url + '/games/top', headers = self.headers)
+        return res.json()
 
 if __name__ == '__main__':
     client_id = ''
-    streamers = ['rhdgurwns', 'rooftopcat99']
+    client_secret = ''
 
-    crawler = Crawler(client_id = client_id, streamers = streamers)
-    crawler.Run()
+    crawler = Crawler(client_id = client_id, client_secret = client_secret)
+    with open('data/targets.json', encoding='utf-8') as f:
+        targets = json.load(f)
+    users = crawler.Users(targets.values())
+    channels = crawler.Channel_by_ID(users)
+    follows = crawler.Follows(users)
+    teams = crawler.Teams(users)
+    videos = crawler.Videos(users)
+    top_game = crawler.Top_Game()
+
+    ###################################################
+    # TODO
+    # Requests which needs oauth
+    #subscribers = crawler.Subscribers(users)
+
+
+    print('a')
