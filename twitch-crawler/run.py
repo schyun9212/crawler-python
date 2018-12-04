b2203ad97f3b35004acd09e0881db63c50cbe6aa
schyun <schyun9212@altair.snu.ac.kr>
2018-12-05 01:07:30 +0900

    modified path

--- twitch-crawler/run.py
+++ twitch-crawler/run.py
@@ -6,8 +6,9 @@ import time
 
 def Run(index_file = 'twitch-index.json',
         out_dir = 'dist'):
+    base_dir = os.path.dirname(__file__)
     try:
-        with open(index_file, encoding='utf-8') as f:
+        with open(os.path.join(base_dir, index_file), encoding='utf-8') as f:
             index = json.load(f)
             client_id = index['client-ID']
             client_secret = ''
@@ -21,9 +22,9 @@ def Run(index_file = 'twitch-index.json',
     new_day = int(t[-4:]) < 30 
 
     crawler = Crawler(client_id = client_id, client_secret = client_secret)
-    with open(index['targets'], encoding='utf-8') as f:
+    with open(os.path.join(base_dir, index['targets']), encoding='utf-8') as f:
         targets = json.load(f)
-    with open(index['filters'], encoding='utf-8') as f:
+    with open(os.path.join(base_dir, index['filters']), encoding='utf-8') as f:
         filters = json.load(f)
     
     users = crawler.Users(targets.values())
@@ -31,19 +32,28 @@ def Run(index_file = 'twitch-index.json',
     #############################################################
     # Collect data with 30 minute period
     streams= crawler.Stream_by_User(users, filters['stream_by_user'])
+    print('streams done')
     live_streams = crawler.Live_Streams()
+    print('live_streams done')
     top_game = crawler.Top_Game()
+    print('top_game done')
     stream_summary = crawler.Stream_Summary()
+    print('stream_summary done')
 
     #############################################################
     # Collect data with 1 day period
 
     if new_day :
         channels = crawler.Channel_by_ID(users, filters['channel_by_id'])
+        print('channels done')
         follows = crawler.Follows(users)
+        print('follows done')
         teams = crawler.Teams(users)
+        print('teams done')
         videos = crawler.Videos(users)
+        print('videos done')
     
+    out_dir = os.path.join(base_dir, out_dir)
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
     
@@ -59,6 +69,7 @@ def Run(index_file = 'twitch-index.json',
             
     with open(out_dir + '/' + t + '.json', 'w', encoding='utf-8') as f:
         json.dump(users, f, ensure_ascii=False)
+        print('end crawling')
 
 if __name__ == '__main__':
-    Run()
+    Run('twitch-index.json', 'dist')
--- youtube-crawler/run.py
+++ youtube-crawler/run.py
@@ -6,8 +6,9 @@ import time
 
 def Run(index_file = 'youtube-index.json',
         out_dir = 'dist'):
+    base_dir = os.path.dirname(__file__)
     try:
-        with open(index_file, encoding='utf-8') as f:
+        with open(os.path.join(base_dir, index_file), encoding='utf-8') as f:
             index = json.load(f)
             api_key = index['api-key']
             targets = index['targets']
@@ -20,22 +21,26 @@ def Run(index_file = 'youtube-index.json',
     t = time.strftime('%y%m%d%H%M')
 
     crawler = Crawler(api_key = api_key)
-    with open(targets, encoding='utf-8') as f:
+    with open(os.path.join(base_dir, targets), encoding='utf-8') as f:
         targets = json.load(f)
     
     output = {}
 
     output['Channels'] = crawler.Channels(targets)
+    print('channels done')
     video_lists = crawler.SearchVideos(targets)
     output['Videos'] = crawler.Videos(targets, video_lists)
+    print('videos done')
 
     #############################################################
     
+    out_dir = os.path.join(base_dir, out_dir)
     if not os.path.exists(out_dir):
         os.mkdir(out_dir)
     
     with open(out_dir + '/' + t + '.json', 'w', encoding='utf-8') as f:
         json.dump(output, f, ensure_ascii=False)
+        print('end crawling')
 
 if __name__ == '__main__':
     Run()
